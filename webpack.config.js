84944236aaea1c9a6964f5871e9bdbfa36272a4c
Dan Williams <dan@331.io>
2015-11-02 23:49:21 -0500

    Added babel, getting ready for hot loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,35 @@
 var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
+var merge = require('webpack-merge');
 
+var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, 'app');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 
-module.exports = {
+var common = {
   entry: APP_PATH,
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: BUILD_PATH,
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: APP_PATH
+      },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: APP_PATH
+      }
+    ]
   },
   plugins: [
     new HtmlwebpackPlugin({
@@ -25,3 +44,18 @@ module.exports = {
     progress: true
   }
 };
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'cheap-eval-source-map',
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true
+    }
+  });
+}
