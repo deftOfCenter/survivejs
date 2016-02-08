6e2a2c79ce41c7be29549a3e5eef329fb6346ca0
Dan Williams <dan@331.io>
2016-02-08 10:15:19 -0500

    Add local storage and persistance framework

--- app/index.jsx
+++ app/index.jsx
@@ -3,14 +3,10 @@ import './main.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import alt from './libs/alt';
+import storage from './libs/storage';
+import persist from './libs/persist';
 
-  // ReactDOM.render(<App />, document.getElementById('app'));
-main();
+persist(alt, storage, 'app');
 
-function main() {
-  const app = document.createElement('div');
-
-  document.body.appendChild(app);
-
-  ReactDOM.render(<App />, app);
-}
+ReactDOM.render(<App />, document.getElementById('app'));
