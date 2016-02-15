9642fdb70b6a034ab0c0b9d9155b328afaa9f6bd
Dan Williams <dan@331.io>
2016-02-14 22:01:57 -0500

    Drag and drop works

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,83 +1,39 @@
 import React from 'react';
+import {DragSource, DropTarget} from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
 
-export default class Note extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // Track 'editing' state/
-    this.state = {
-      editing: false
+const noteSource = {
+  beginDrag(props) {
+    return {
+      id: props.id
     };
   }
-  render() {
-    // Render the component differently based on state.
-    if(this.state.editing) {
-      return this.renderEdit();
-    }
+};
 
-    return this.renderNote();
+const noteTarget = {
+  hover(targetProps, monitor) {
+    const targetId = targetProps.id;
+    const sourceProps = monitor.getItem();
+    const sourceId = sourceProps.id;
+
+    if(sourceId !== targetId) {
+      targetProps.onMove({sourceId, targetId});
+    }
   }
-  renderEdit = () => {
-    // We deal with blur and input handlers here. These map to DOM events.
-    // We also set selection to input end using a callback at a ref.
-    // It gets triggered after the component is mounted.
-    //
-    // We could also use a string reference (i.e., `ref="input") and
-    // then refer to the element in question later in the code. This
-    // would allow us to use the underlying DOM API through
-    // this.refs.input. This can be useful when combined with
-    // React lifecycle methods.
-    return <input type="text"
-      ref={
-        (e) => e ? e.selectionStart = this.props.task.length : null
-      }
-      autoFocus={true}
-      defaultValue={this.props.task}
-      onBlur={this.finishEdit}
-      onKeyPress={this.checkEnter} />;
-  };
-  renderNote = () => {
-    const onDelete = this.props.onDelete;
+};
 
-    return (
-      <div onClick={this.edit}>
-        <span className="note__task">{this.props.task}</span>
-        {onDelete ? this.renderDelete() : null }
-      </div>
-    );
-  };
-  renderDelete = () => {
-    return <button className="note__delete" onClick={this.props.onDelete}>x</button>;
-  };
-  edit = () => {
-    // Enter edit mode
-    this.setState({
-      editing: true
-    });
-  };
-  checkEnter = (e) => {
-    // The user hit 'enter', let's finish up
-    if(e.key === 'Enter') {
-      this.finishEdit(e);
-    }
-  };
-  finishEdit = (e) => {
-    // `Note` will trigger an optional `onEdit` callback once it
-    // has a new value. We will use this to communicate the change to
-    // `App`.
-    //
-    // A smarter way to deal with the default value would be to set // it through `defaultProps`.
-    //
-    // See *Typing with React* chapter for more information.
-    const value = e.target.value;
+@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
+  connectDragSource: connect.dragSource()
+}))
+@DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
+  connectDropTarget: connect.dropTarget()
+}))
+export default class Note extends React.Component {
+  render() {
+    const {connectDragSource, connectDropTarget, id, onMove, ...props} = this.props;
 
-    if(this.props.onEdit && value.trim()) {
-      this.props.onEdit(value);
-
-      // Exit edit mode.
-      this.setState({
-        editing: false
-      });
-    }
-  };
+    return connectDragSource(connectDropTarget(
+      <li {...props}>{props.children}</li>
+    ));
+  }
 }
