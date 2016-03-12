8c551e5d61388fc8ad22a195fb43555218156603
Dan Williams <dan@331.io>
2016-03-12 16:35:57 -0500

    streamline the build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
 const path = require('path');
-// const HtmlwebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 const NpmInstallPlugin = require('npm-install-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// Load *package.json* so we can use `dependencies` from there
+const pkg = require('./package.json');
 
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build'),
+  style: path.join(__dirname, 'app/main.css')
 };
 
 process.env.BABEL_ENV = TARGET;
 
 var common = {
-  entry: PATHS.app,
+  entry: {
+    app: PATHS.app,
+    style: PATHS.style
+  },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
-  output: {
-    path: PATHS.build,
-    filename: 'bundle.js'
-  },
   module: {
     loaders: [
-      {
-        test: /\.css$/,
-        loaders: ['style', 'css'],
-        include: PATHS.app
-      },
       {
         test: /\.jsx?$/,
         // Enable caching for improved performance during development
@@ -40,18 +40,20 @@ var common = {
       }
     ]
   },
-  // plugins: [
-  //   new HtmlwebpackPlugin({
-  //     title: 'Kanban app'
-  //   })
-  // ]
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'node_modules/html-webpack-template/index.ejs',
+      title: 'Kanban app',
+      appMountId: 'app',
+      inject: false
+    })
+  ]
 };
 
 if(TARGET === 'start' || !TARGET) {
   module.exports = merge(common, {
     devtool: 'eval-source-map',
     devServer: {
-      contentBase: PATHS.build,
 
       historyApiFallback: true,
       hot: true,
@@ -71,6 +73,16 @@ if(TARGET === 'start' || !TARGET) {
       host: process.env.HOST,
       port: process.env.PORT
     },
+    module: {
+      loaders: [
+        // Define development specific CSS setup
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+          include: PATHS.app
+        }
+      ]
+    },
     plugins: [
       new webpack.HotModuleReplacementPlugin(),
       new NpmInstallPlugin({
@@ -80,9 +92,43 @@ if(TARGET === 'start' || !TARGET) {
   });
 }
 
-if(TARGET === 'build') {
+if(TARGET === 'build' || TARGET == 'stats') {
   module.exports = merge(common, {
+    // Define vendor entry pont needed for spitting
+    entry: {
+      vendor: Object.keys(pkg.dependencies).filter(function(v) {
+        // Exclude alt-utils as it won't work with this setup
+        // due to the way the package has been designed
+        // (no package.json main).
+        return v !== 'alt-utils';
+      })
+    },
+    output: {
+      path: PATHS.build,
+      // Output using entry name
+      filename: '[name].[hash].js',
+      chunkFilename: '[chunkhash].js'
+    },
+    module: {
+      loaders: [
+        // Extract CSS during setup
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style', 'css'),
+          include: PATHS.app
+        }
+      ]
+    },
     plugins: [
+      new CleanPlugin([PATHS.build], {
+        verbose: false // Dont write logs to console
+      }),
+      // Output extractes CSS to a files
+      new ExtractTextPlugin('[name].[chunkhash].css'),
+      // Extract vendor and manifest files
+      new webpack.optimize.CommonsChunkPlugin({
+        names: ['vendor', 'manifest']
+      }),
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': '"production"'
 
